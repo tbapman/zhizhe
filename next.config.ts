4d2687ba3538eaa527926fce6651360deb68fe11
liqi <tbapman@163.com>
2025-10-16 23:38:20 +0800

    fix(next.config.ts): 调整

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // ✅ 忽略 ESLint 错误和警告
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
