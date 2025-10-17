3b78245cbdd70612f25fd8550a4d55ac99dab686
liqi <tbapman@163.com>
2025-10-17 09:33:10 +0800

    fix(代码): 修复

--- src/lib/db/mongoose.ts
+++ src/lib/db/mongoose.ts
@@ -1,11 +1,5 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI!;
-
-if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
-}
-
 interface MongooseCache {
   conn: typeof mongoose | null;
   promise: Promise<typeof mongoose> | null;
@@ -27,6 +21,12 @@ async function dbConnect() {
   }
 
   if (!cached!.promise) {
+    const MONGODB_URI = process.env.MONGODB_URI;
+
+    if (!MONGODB_URI) {
+      throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
+    }
+
     const opts = {
       bufferCommands: false,
     };
