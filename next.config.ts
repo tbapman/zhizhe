e7f3e3510e755fa39575a3818fae49c87075b49c
liqi <tbapman@163.com>
2025-10-17 09:52:10 +0800

    fix(多余配置): 删除

--- next.config.ts
+++ next.config.ts
@@ -6,9 +6,6 @@ const nextConfig: NextConfig = {
     // ✅ 忽略 ESLint 错误和警告
     ignoreDuringBuilds: true,
   },
-  env: {
-    MONGODB_URI: process.env.MONGODB_URI,
-  },
 };
 
 export default nextConfig;
